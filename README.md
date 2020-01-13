c39929f7cde3821be682ef28a5714e8c8662e1ae
e7now <e7now@yandex.ru>
2020-01-13 23:57:47 +0700

    Init project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cft_sort_file_content
 This project is a code example for Center of Financial Technologies
+
+Task is described in spec/test task.docx file.
---
+++ src/main/java/ru/eisemenov/sort_file_content/Main.java
@@ -0,0 +1,7 @@
+package ru.eisemenov.sort_file_content;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
